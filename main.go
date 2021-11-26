76e95d3532f1c8af032254eb7c4db8e7ffe02ed4
Gibson Chikafa <gibbschikafa@gmail.com>
2021-11-26 11:24:30 +0100

    tmp

--- main.go
+++ main.go
@@ -3,64 +3,34 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/go-git/go-git/v5"
 	. "github.com/go-git/go-git/v5/_examples"
-	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
-// Basic example of how to commit changes to the current branch to an existing
-// repository.
+// Basic example of how to clone a repository using clone options.
 func main() {
-	CheckArgs("<directory>")
-	directory := os.Args[1]
+	CheckArgs("<url>", "<directory>")
+	url := os.Args[1]
+	directory := os.Args[2]
 
-	// Opens an already existing repository.
-	r, err := git.PlainOpen(directory)
-	CheckIfError(err)
+	// Clone the given repository to the given directory
+	Info("git clone %s %s --recursive", url, directory)
 
-	w, err := r.Worktree()
-	CheckIfError(err)
-
-	// We can verify the current status of the worktree using the method Status.
-	Info("git status --porcelain")
-	status, err := w.Status()
-	CheckIfError(err)
-	fmt.Println(status)
-
-	err = w.AddWithOptions(&git.AddOptions{All: true})
-	CheckIfError(err)
-
-	Info("git status --porcelain. After add")
-	s, err := w.Status()
-	CheckIfError(err)
-	fmt.Println(s)
-
-	// Commits the current staging area to the repository, with the new file
-	// just created. We should provide the object.Signature of Author of the
-	// commit Since version 5.0.1, we can omit the Author signature, being read
-	// from the git config files.
-	Info("git commit -m \"example go-git commit\"")
-	commit, err := w.Commit("example go-git commit", &git.CommitOptions{
-		Author: &object.Signature{
-			Name:  "John Doe",
-			Email: "john@doe.org",
-			When:  time.Now(),
-		},
+	r, err := git.PlainClone(directory, false, &git.CloneOptions{
+		URL:               url,
+		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
 	})
 
 	CheckIfError(err)
 
-	// Prints the current HEAD to verify that all worked well.
-	Info("git show -s")
-	obj, err := r.CommitObject(commit)
+	// ... retrieving the branch being pointed by HEAD
+	Info("Retrieving the branch being pointed by HEAD")
+	ref, err := r.Head()
 	CheckIfError(err)
-	fmt.Println(obj)
-
-	// We can verify the current status of the worktree using the method Status.
-	Info("git status --porcelain. After commit")
-	status2, err := w.Status()
+	Info("Retrieving the commit object")
+	commit, err := r.CommitObject(ref.Hash())
 	CheckIfError(err)
-	fmt.Println(status2)
+	Info("Commit object is: ")
+	fmt.Println(commit)
 }
